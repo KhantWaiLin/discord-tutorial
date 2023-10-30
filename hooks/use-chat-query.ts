ebdc1fe8dfe7220ed638317ecbc7cddcf766b6f4
KhantWaiLin <khantkhant.cm@gmail.com>
2023-10-30 10:52:07 +0630

    fixing use-chat-query.ts typescript error

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -18,7 +18,7 @@ export const useChatQuery = ({
 }: ChatQueryProps) => {
   const { isConnected } = useSocket();
 
-  const fetchMessages = async ({ pageParam = undefined }) => {
+  const fetchMessages: any = async ({ pageParam = undefined }) => {
     const url = qs.stringifyUrl(
       {
         url: apiUrl,
@@ -39,7 +39,7 @@ export const useChatQuery = ({
       initialPageParam: 1,
       queryKey: [queryKey],
       queryFn: fetchMessages,
-      getNextPageParam: (lastPage) => lastPage?.nextCursor,
+      getNextPageParam: (lastPage: any) => lastPage?.nextCursor,
       refetchInterval: isConnected ? false : 1000,
     });
 
